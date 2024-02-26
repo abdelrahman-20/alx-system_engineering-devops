5376a1bef60e28eeadb439059d8b724af9c14256
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-02-25 18:59:33 -0800

    Advanced Tasks

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,17 +2,17 @@
 # A Script To Use Puppet
 
 file { '/etc/ssh/ssh_config':
-  ensure => present,
+  ensure  => present,
 }
 
 file_line { 'Turn off passwd auth':
-  path	=> '/etc/ssh/ssh_config',
-  line	=> 'PaswordAuthentication no',
-  match	=> '^#PaswordAuthentication',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PaswordAuthentication no',
+  match	  => '^#PaswordAuthentication',
 }
 
 file_line { 'Declare identity file':
-  path	=> '/etc/ssh/ssh_config',
-  line	=> 'IdentityFile ~/.ssh/school',
-  match	=> '^#IdentityFile',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^#IdentityFile',
 }
