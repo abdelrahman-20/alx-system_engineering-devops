65fd03f3bc434f0e6e78adae876229a2cd58d7d5
abdelrahman-20 <abdo.alamear25@gmail.com>
2024-08-22 03:00:42 -0700

    Task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+import re
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    emp_id = sys.argv[1]
+
+    user = requests.get(f"{url}/users/{emp_id}").json()
+
+    params = {"userId": emp_id}
+    todos = requests.get(f"{url}/todos", params=params).json()
+
+    completed = [x.get("title") for x in todos if x.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(complete)) for complete in completed]
